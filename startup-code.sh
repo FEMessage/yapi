780c691e2ef24ce7d751809d698c75c7008f261a
“GammaGo <msze9667@gmail.com>
2018-12-24 11:59:54 +0800

    feat: docker 部署

---
+++ startup-code.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd `dirname $0`
+#更新代码
+git pull
+
+#下载依赖
+#docker run --rm -v "$PWD":/root -w /root node npm install -g python && npm install
+
+#构建镜像
+docker build --rm -t deepexi/yapi .
+
+#删除容器
+docker rm -f yapi &> /dev/null
+
+docker run -d --restart=on-failure:5 \
+    -w /root \
+    -e GATEWAY_HOST=119.29.28.59 \
+    -e DB_HOST=112.74.61.59 \
+    -e DB_PORT=3306 \
+    -e DB_NAME=pmsX \
+    -e DB_USERNAME=root \
+    -e DB_PASSWORD=my-secret-ab \
+    -e TZ=Asia/Shanghai \
+    -p 3000:3000 \
+    -v $PWD/logs/:/root/logs/  \
+    -v $PWD/run/:/root/run/  \
+    -v $PWD/node_modules/:/root/node_modules/  \
+    --name yapi deepexi/yapi
